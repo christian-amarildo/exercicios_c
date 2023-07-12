ac2793cbe4cdf9c18bfce7bc4f0bd50ae4d506f8
christian-amarildo <127919702+christian-amarildo@users.noreply.github.com>
2023-07-11 23:32:27 -0300

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+main()
+{
+int x;
+for (x=1;x <=10; x++)
+printf("\nCurso de Redes ");
+printf("\n");
+system("pause");
+return 0;
+} 
